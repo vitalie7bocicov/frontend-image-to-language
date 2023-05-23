2b1d4e85b01ed013898ec171fd67b6dad28e3781
eduardsoltan <eduardsoltan@gmail.com>
2023-05-23 21:38:03 +0200

    change

--- image-to-language/src/lib/firebaseInit.js
+++ image-to-language/src/lib/firebaseInit.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
+
+console.log(process.env.REACT_APP_API_KEY);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
