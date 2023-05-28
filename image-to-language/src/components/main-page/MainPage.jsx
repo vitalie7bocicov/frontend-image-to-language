4d4f6e6c0c415ff84f8f9544cf28658ab9aec204
vitalie7bocicov <vitalie.bocicov99@gmail.com>
2023-05-28 15:02:21 +0300

    minor ui update on history component and photo upload validation added

--- image-to-language/src/components/main-page/MainPage.jsx
+++ image-to-language/src/components/main-page/MainPage.jsx
@@ -47,62 +47,73 @@ export default function MainPage() {
 
   return (
     <>
-    <Navbar />
-    <div className="container mainDiv2 d-flex justify-content-center">
-      <div className="centerDiv col-12 col-sm-10 col-md-8 col-lg-6 my-auto">
-        <h1>PhotoSpeak</h1>
-        <div>
-          <Formik
-            initialValues={{ image: null, language: "english" }}
-            onSubmit={onSubmit}
-            validateOnBlur={false}
-            validateOnChange={false}
-          >
-            {(props) => (
-              <Form>
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                  <fieldset className="form-group col-4 my-4">
-                    <h4>Choose a language!</h4>
-                    <div className="d-flex align-items-center">
+      <Navbar />
+      <div className="container mainDiv2 d-flex justify-content-center">
+        <div className="centerDiv col-12 col-sm-10 col-md-8 col-lg-6 my-auto">
+          <h1>PhotoSpeak</h1>
+          <div>
+            <Formik
+              initialValues={{ language: "english" }}
+              onSubmit={onSubmit}
+              validateOnBlur={false}
+              validateOnChange={false}
+              validate={(values) => {
+                const errors = {};
+                if (!values.image) {
+                  errors.image = "Please upload an image";
+                }
+                return errors;
+              }}
+            >
+              {(props) => (
+                <Form>
+                  <div className="d-flex flex-column justify-content-center align-items-center">
+                    <fieldset className="form-group col-4 my-4">
+                      <h4>Choose a language!</h4>
+                      <div className="d-flex align-items-center">
+                        <Field
+                          name="language"
+                          as="select"
+                          className="form-control"
+                          onChange={(e) => {
+                            setLanguage(e.target.value);
+                          }}
+                          value={language}
+                        >
+                          <option value="en">English</option>
+                          <option value="ro">Romanian</option>
+                          <option value="zh">Chinese</option>
+                          <option value="es">Spanish</option>
+                          <option value="hi">Hindi</option>
+                          <option value="ar">Arabic</option>
+                          <option value="pt">Portuguese</option>
+                          <option value="bn">Bengali</option>
+                          <option value="ru">Russian</option>
+                          <option value="ja">Japanese</option>
+                          <option value="de">German</option>
+                          <option value="fr">French</option>
+                        </Field>
+                      </div>
+                    </fieldset>
+                    <h4>Upload a picture!</h4>
+                    <fieldset className="form-group col-4">
                       <Field
-                        name="language"
-                        as="select"
-                        className="form-control"
-                        onChange={(e) => {
-                          setLanguage(e.target.value);
-                        }}
-                        value={language}
-                      >
-                        <option value="en">English</option>
-                        <option value="ro">Romanian</option>
-                        <option value="zh">Chinese</option>
-                        <option value="es">Spanish</option>
-                        <option value="hi">Hindi</option>
-                        <option value="ar">Arabic</option>
-                        <option value="pt">Portuguese</option>
-                        <option value="bn">Bengali</option>
-                        <option value="ru">Russian</option>
-                        <option value="ja">Japanese</option>
-                        <option value="de">German</option>
-                        <option value="fr">French</option>
-                      </Field>
-                    </div>
-                  </fieldset>
-                  <h4>Upload a picture!</h4>
-                  <fieldset className="form-group col-4">
-                    <Field name="image" component={ImageUploadField} />
-                  </fieldset>
-                  {props.errors.image && <div>{props.errors.image}</div>}
-                  <button className="findButton" type="submit">
-                    Find Words
-                  </button>
-                </div>
-              </Form>
-            )}
-          </Formik>
+                        name="image"
+                        component={ImageUploadField}
+                        required
+                      />
+                    </fieldset>
+                    {props.errors.image && <div>{props.errors.image}</div>}
+                    <button className="findButton" type="submit">
+                      Find Words
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
