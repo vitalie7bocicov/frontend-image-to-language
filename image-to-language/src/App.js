5bb71ca9be3e4ac496bebf6a1f1f81f29e34e7f8
vitalie7bocicov <vitalie.bocicov99@gmail.com>
2023-05-28 16:50:44 +0300

    redirect to sign-in if not authenticated

--- image-to-language/src/App.js
+++ image-to-language/src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import MainPage from "./components/main-page/MainPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import GeneratedPage from "./components/generated-page/GeneratedPage";
 import SignUpPage from "./components/signUp-page.js/SignUpPage";
 import SignInPage from "./components/signIn-page/SignInPage";
 import ConfirmEmailPage from "./components/email-confirmation/EmailConfiramtionPage";
 import HistoryPage from "./components/history-page/HistoryPage";
 import GeneratedPageFromHistory from "./components/generated-page-from-history/GeneratedPageFromHistory";
+import { getCurrentUser } from "./services/authSerive";
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -19,8 +19,21 @@ function App() {
           <Route path="/confirm-email" element={<ConfirmEmailPage />} />
           <Route path="/sign-in" element={<SignInPage />} />
           <Route path="/generated-page" element={<GeneratedPage />} />
-          <Route path="/generated-page-from-history" element={<GeneratedPageFromHistory />} />
+          <Route
+            path="/generated-page-from-history"
+            element={<GeneratedPageFromHistory />}
+          />
           <Route path="/history" element={<HistoryPage />} />
+          <Route
+            path="/*"
+            element={
+              getCurrentUser() ? (
+                <Navigate to="/" />
+              ) : (
+                <Navigate to="/sign-in" />
+              )
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
